0343be6defcdcb430e90b3ea014956b7830ecb95
Pranay Sanjay Mokal <pranaymokal77@gmail.com>
2026-06-14 00:09:53 +0530

    added multilingual support

---
+++ chunk-DXV7O6UG.js
@@ -0,0 +1,1 @@
+import{C as a,da as o}from"./chunk-CJZTVEPE.js";import{$ as t,tb as r}from"./chunk-BRBZC7YT.js";var n=(()=>{class e{static \u0275fac=function(i){return new(i||e)};static \u0275mod=r({type:e});static \u0275inj=t({imports:[o,a,o]})}return e})();export{n as a};
